65cbb1182a32cc9b02bf91c206cf47e63ffaa1cf
YaroFang <@>
2018-09-01 00:44:07 +0800

    int test

---
+++ src/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args)
+    {
+        int a=10;
+        byte b = 100;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("not Hello World!");
+    }
+}
